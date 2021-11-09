6dea9386bcf9b36911828b31b8fd720a60e2692a
anukarumanthra84 <anu.oct20@gmail.com>
2021-11-09 05:13:00 +0000

    RMS 123-New Feature added

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 public class JDBCDemo {
     public static void main(String[] args)throws Exception {
+        System.out.println("New Feature added from RMS123");
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url="jdbc:mysql://localhost:3306/mydb";
         String uname="root";
